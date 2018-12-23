c750b9c6da77eef294c9012e7acd57f3e1f6ceb7
radu <radu@yahoo.com>
2018-12-24 00:21:46 +0200

    Update sql scripts to insert random data

--- SQL_scripts/InsertData.sql
+++ SQL_scripts/InsertData.sql
@@ -1,6 +1,27 @@
-insert into tree (name,PID,height) values ('root',0,0);
-insert into tree (name,PID,height) values ('ch1',1,1);
-insert into tree (name,PID,height) values ('ch2',1,1);
-insert into tree (name,PID,height) values ('ch11',2,2);
-insert into tree (name,PID,height) values ('ch12',2,2);
-insert into tree (name,PID,height) values ('ch21',3,2);
+SET sql_mode = '';
+
+DELIMITER $$
+CREATE PROCEDURE prepare_data_r18()
+BEGIN
+  DECLARE i INT DEFAULT 6;
+  DECLARE pid int;
+  WHILE i < 100 DO
+    SET pid =rand()*(i-1)+1;
+    set @tec='node'+'-'+cast(i as char(10));
+    SELECT @height:= height from tree where ID=pid;
+    INSERT INTO tree (name,PID,height) VALUES (concat('node-',concat(i, '')),pid,@height+1);
+    SET i = i + 1;
+  END WHILE;
+END$$
+DELIMITER ;
+
+
+insert into tree (name,PID,height) values ('root-0',0,0);
+insert into tree (name,PID,height) values ('node-1',1,1);
+insert into tree (name,PID,height) values ('node-2',1,1);
+insert into tree (name,PID,height) values ('node-3',2,2);
+insert into tree (name,PID,height) values ('node-4',2,2);
+insert into tree (name,PID,height) values ('node-5',3,2);
+
+
+CALL prepare_data_r18();
